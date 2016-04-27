8186cea294bb39e74923eee7f1f33bdf614936da
wenigustirahayu <wenigr97@gmail.com>
2016-04-27 11:45:11 +0700

    menambahkan inputan nilai 1 dan nilai 2

--- src/tugas/bab5/MainPerhitungan.java
+++ src/tugas/bab5/MainPerhitungan.java
@@ -6,6 +6,12 @@ public class MainPerhitungan {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("======= PROGRAM PERHITUNGAN =======");
+        System.out.println("");
+        System.out.print("-Masukkan nilai 1 : ");
+        int bil1 = in.nextInt();
+        System.out.print("-Masukkan nilai 2 : ");
+        int bil2 = in.nextInt();
 
     }
 }
