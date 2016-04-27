98af30cfc0cd0c18f0d36487fa1ce32d2001ecd7
wenigustirahayu <wenigr97@gmail.com>
2016-04-27 12:30:35 +0700

    mainJaket

---
+++ src/tugas/bab5/mainJaket.java
@@ -0,0 +1,14 @@
+package mainjaket;
+
+import java.util.Scanner;
+
+public class mainJaket {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int pilihan;
+        System.out.println("                        CV. Labkomdas            ");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Membeli >100 pcs untuk masing-masing jaket akan mendapatkan diskon!");       
+    }
+}
