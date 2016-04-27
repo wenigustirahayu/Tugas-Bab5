2a28e2b9ef6385ec6d31d99569f04eff57b37e28
wenigustirahayu <wenigr97@gmail.com>
2016-04-27 12:35:09 +0700

    menambahkan perulangan do while untuk pemilihan jaket

--- src/tugas/bab5/mainJaket.java
+++ src/tugas/bab5/mainJaket.java
@@ -9,6 +9,18 @@ public class mainJaket {
         int pilihan;
         System.out.println("                        CV. Labkomdas            ");
         System.out.println("-------------------------------------------------------------------");
-        System.out.println("Membeli >100 pcs untuk masing-masing jaket akan mendapatkan diskon!");       
+        System.out.println("Membeli >100 pcs untuk masing-masing jaket akan mendapatkan diskon!");
+        do {
+            System.out.println("Tipe Jaket");
+            System.out.println("1. Jaket A : Rp 100.000/pcs    >100 : Rp 95.000/pcs");
+            System.out.println("2. Jaket B : Rp 125.000/pcs    >100 : Rp 120.000/pcs");
+            System.out.println("3. Jaket C : Rp 175.000/pcs    >100 : Rp 160.000/pcs");
+            System.out.println("0. Keluar");
+            System.out.print("Pilihan Jaket: ");
+            pilihan = in.nextInt();
+            switch (pilihan) {
+                
+            }
+        } while (pilihan != 0);
     }
 }
