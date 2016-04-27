b349ad0aed26da0311957efa4be0b798c67e75e3
wenigustirahayu <wenigr97@gmail.com>
2016-04-27 11:28:18 +0700

    penambahan method +,-,*,/

--- src/tugas/bab5/Perhitungan.java
+++ src/tugas/bab5/Perhitungan.java
@@ -4,5 +4,25 @@ public class Perhitungan {
 
     private int pembilang, penyebut;
 
+    public static void Penjumlahan(int a, int b) {
+        int jumlah = a + b;
+        System.out.println("[1]... Hasil penjumlahan    : " + jumlah);
     }
+
+    public static void Pengurangan(int a, int b) {
+        int kurang = a - b;
+        System.out.println("[2]... Hasil pengurangan    : " + kurang);
+    }
+
+    public void Perkalian(int a, int b) {
+        int kali = a * b;
+        System.out.println("[3]... Hasil perkalian      : " + kali);
+    }
+
+    public void Pembagian(double a, double b) {
+        double bagi = a / b;
+        System.out.println("[4]... Hasil pembagian      : " + bagi);
+    }
+
+   
 }
