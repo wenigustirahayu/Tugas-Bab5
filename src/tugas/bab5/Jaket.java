ceb2bbfee8447694c358977e7ae7d3b8ab094610
wenigustirahayu <wenigr97@gmail.com>
2016-04-27 12:17:44 +0700

    Penambahan method hitungDiskon untuk Jaket A

--- src/tugas/bab5/Jaket.java
+++ src/tugas/bab5/Jaket.java
@@ -6,4 +6,16 @@ public class Jaket {
     private final int jaketB = 125000;
     private final int jaketC = 175000;
 
+    public void hitungDiskonA(int beliA) {
+        if (beliA > 100) {
+            int harga = beliA * 95000;
+            System.out.println("Selamat, anda mendapatkan Diskon.");
+            System.out.println("Jaket A : Rp 95.000/pcs");
+            System.out.println("Total Harga: " + harga);
+        } else if (beliA <= 100) {
+            int harga = beliA * jaketA;
+            System.out.println("Total Harga : " + harga);
+        }
+    }
 }
+
